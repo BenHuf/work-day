19795e570168aeec8a1375c453a72cc6d4af5c67
BenHuf <benthehuffman@gmail.com>
2022-05-08 09:12:55 -0400

    added save button and task divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,48 @@
 var $container = $(".container");
 
 var hourCount = 9;
-var now = moment().format("hhA");
+var now = moment().format("kk");
 
 console.log("the time is" + now);
 
+// creates time block elements 
+var createTimeBlocks = function() {
 for (var i = 0; i < hourCount; i++) {
-    var time = i + 9
-    console.log(time);
-    if (i < 4) {
-        $('<div class="timeblock col-12 col-lg border border-primary p-4">')
-        .text(time + "AM")
+    var timeblockTime = moment().hour(9).add(i, 'hour').format("hA");
+    var hour = moment().hour(9).add(i, 'hour').format("kk")
+    console.log(hour);
+    console.log(timeblockTime);
+    console.log("now" + now + "hour" + hour)
+    if (hour < now) {
+        $('<div class="d-flex bg-secondary row justify-content-between timeblock col-12 col-lg border border-primary p-4">')
+        .text(timeblockTime)
+        .attr('id', hour)
+        .appendTo($container);
+    }
+    else if (hour === now) {
+        $('<div class="d-flex bg-danger row justify-content-between timeblock col-12 col-lg border border-primary p-4">')
+        .text(timeblockTime)
+        .attr('id', hour)
         .appendTo($container);
     }
     else {
-        time -= 12;
-        $('<div class="timeblock col-12 col-lg border border-primary p-4">')
-        .text(time + "PM")
+        $('<div class="d-flex bg-success row justify-content-between timeblock col-12 col-lg border border-primary p-4">')
+        .text(timeblockTime)
+        .attr('id', hour)
         .appendTo($container);
     }
+}
+
+// adds task text div
+$('<div class=toDo>')
+.text("add text here")
+.attr('id', parent)
+.appendTo(".timeblock");
+
+// add save button
+$('<button class=saveBtn>')
+.text("Save")
+.appendTo($(".timeblock"));
 };
 
-var time = moment("hh");
-console.log("this is the" + time);
+createTimeBlocks();
