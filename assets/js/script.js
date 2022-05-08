55dc2715a8624b3538960dda6a59bab04660da3c
BenHuf <benthehuffman@gmail.com>
2022-05-07 23:34:55 -0400

    dynamically generate time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,25 @@
-var $containerEl = $('.container');
+var $container = $(".container");
 
-var hourCount = 8;
+var hourCount = 9;
+var now = moment().format("hhA");
 
-// for (var i = 0; i < hourCount; i++) {
-//     var time = x;
-// };
+console.log("the time is" + now);
+
+for (var i = 0; i < hourCount; i++) {
+    var time = i + 9
+    console.log(time);
+    if (i < 4) {
+        $('<div class="timeblock col-12 col-lg border border-primary p-4">')
+        .text(time + "AM")
+        .appendTo($container);
+    }
+    else {
+        time -= 12;
+        $('<div class="timeblock col-12 col-lg border border-primary p-4">')
+        .text(time + "PM")
+        .appendTo($container);
+    }
+};
 
 var time = moment("hh");
-console.log(time);
+console.log("this is the" + time);
